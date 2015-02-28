51f0f5eec69c5dc29626999656cf32e2c45e9dcb
sairus2k <sairus2k@gmail.com>
2015-02-28 19:36:57 +0700

    Add interactivity into portfolio menu

--- scrypt.js
+++ scrypt.js
@@ -2,5 +2,33 @@ var main = function () {
   $('.banner').unslider({
     dots: true
   });
+  $('.portfolio li div').click(function () {
+    var category = $(this).attr('class');
+    $('.portfolio li div').removeClass('active');
+    $(this).addClass("active");
+    switch (category) {
+    case "btn-all":
+      $('.work>div').addClass('selected');
+      break;
+    case "btn-brand":
+      $('.work div').removeClass('selected');
+      $('.branding').addClass('selected');
+      break;
+    case "btn-web":
+      $('.work div').removeClass('selected');
+      $('.webDesign').addClass('selected');
+      break;
+    case "btn-ui":
+      $('.work div').removeClass('selected');
+      $('.mobileUi').addClass('selected');
+      break;
+    case "btn-pic":
+      $('.work div').removeClass('selected');
+      $('.illustrations').addClass('selected');
+      break;
+    }
+  });
+
+
 };
 $(document).ready(main);
